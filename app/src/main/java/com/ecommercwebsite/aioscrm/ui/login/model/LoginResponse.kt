1f39f486836521d67f3069c26cc7331be914c0b9
Kishan Trambadiya <kishan.aaosa@gmail.com>
2024-04-29 23:35:20 +0530

    Working on fill attendance screen

--- app/src/main/java/com/ecommercwebsite/aioscrm/ui/login/model/LoginResponse.kt
+++ app/src/main/java/com/ecommercwebsite/aioscrm/ui/login/model/LoginResponse.kt
@@ -1,6 +1,8 @@
 package com.ecommercwebsite.aioscrm.ui.login.model
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class LoginResponse(
